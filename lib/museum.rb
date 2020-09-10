d2165fa8317e46c2f055d2481fd655fd5db89364
Jose Lopez <joselopez11394@gmail.com>
2020-09-10 11:50:13 -0400

    adds patrons and admit methods to museum class

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,9 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -15,5 +16,9 @@ class Museum
         exhibit.name == interest
       end
     end.flatten
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 end
