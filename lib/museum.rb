ead10ad37c82bb9a90909eea9dc18efe9539b4f6
Jose Lopez <joselopez11394@gmail.com>
2020-09-10 12:20:20 -0400

    adds ticket_lottery_contestants method in museum class

--- lib/museum.rb
+++ lib/museum.rb
@@ -36,4 +36,14 @@ class Museum
     end
     patrons_by_exhibit
   end
+
+  def ticket_lottery_contestants(exhibit)
+    poor_patrons = []
+    @patrons.each do |patron|
+      if patron.spending_money < exhibit.cost
+        poor_patrons << patron
+      end
+    end
+    poor_patrons
+  end
 end
